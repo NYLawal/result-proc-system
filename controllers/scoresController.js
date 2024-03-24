c0de1f0cd67f6bc6e0364b89752a6d248f9271bc
Nurat Lawal <nurahawwal@yahoo.com>
2024-03-24 22:04:15 +0100

    error handling with scores input

--- controllers/scoresController.js
+++ controllers/scoresController.js
@@ -16,12 +16,12 @@ const addScores = async (req, res, next) => {
   // check whether details match any student of the school
   const isStudent = await Student.findOne({
     $and: [{ admNo }, { firstName }, { lastName }]
-  })
-    .select("admNo firstName lastName");
+  }).select("admNo firstName lastName");
+
   if (!isStudent) {
     return next(new Error("Error: no such student found"));
   }
-  dbDebugger(isStudent._id)
+  // dbDebugger(isStudent._id)
 
   // check whether student exists in the scores database
   //if not, create a document for the student and push scores
@@ -29,26 +29,39 @@ const addScores = async (req, res, next) => {
   if (!alreadyHasScores) {
     const addStudent = await Score.create({ ...req.body, studentId: isStudent._id, AdmissionNumber: isStudent.admNo, Name: isStudent.firstName + " " + isStudent.lastName });
     addStudent.scores.push(req.body)
+    addStudent.scores[0].subjects.push(req.body.subject)
     addStudent.save()
+    dbDebugger("after save")
     res.status(201).json({ status: "success", addStudent, msg: "student created and scores added successfully" });
   }
-  else {// if yes, check whether student has scores for the session and term specified, display error if there is
-    for (let count = 0; count < alreadyHasScores.scores.length; count++) {
-      if (req.body.session === alreadyHasScores.scores[count].session && req.body.term === alreadyHasScores.scores[count].term) {
-        dbDebugger("found ")
-        return next(new Error("Error: scores for this term already exists for the student"));
+  else {// if yes, check whether student has the subject's scores for the session and term specified, display error if there is
+    for (let scorescount = 0; scorescount < alreadyHasScores.scores.length; scorescount++) {
+      if (req.body.session === alreadyHasScores.scores[scorescount].session && req.body.term === alreadyHasScores.scores[scorescount].term) {
+        for (let subjectcount = 0; subjectcount < alreadyHasScores.scores[scorescount].subjects.length; subjectcount++) {
+          if (req.body.subject.subjectName === alreadyHasScores.scores[scorescount].subjects[subjectcount].subjectName) {
+              dbDebugger(" subject match found ")
+              return next(new Error(`Error: ${req.body.subject.subjectName} score for this term already exists for the student`));
+            }
+          } // if score doesn't exist add score for the subject 
+            alreadyHasScores.scores[scorescount].subjects.push(req.body.subject)
+            alreadyHasScores.save()
+            res.status(201).json({ status: "success", msg: `${req.body.subject.subjectName} scores for this student added successfully`});
+          return
       }
-    }
-    // if not, push the scores into the array of student's scores
+    } // if no match is found for the session and term, add score to the new session and term
     alreadyHasScores.scores.push(req.body)
+    const arrayLength = alreadyHasScores.scores.length
+    alreadyHasScores.scores[arrayLength-1].subjects.push(req.body.subject)
     alreadyHasScores.save()
-    res.status(201).json({ status: "success", msg: "scores for this student added successfully" });
+    res.status(201).json({ status: "success", msg: 'scores for this term added successfully for student'});
   }
+    // if not, push the scores into the array of student's scores
+    
 }
 
 const getScores = async (req, res, next) => {
   const { admNo, firstName, lastName } = req.query;
- // check whether details match any student of the school
+  // check whether details match any student of the school
   const isStudent = await Student.findOne({
     $and: [{ admNo }, { firstName }, { lastName }]
   })
@@ -80,6 +93,45 @@ const promoteStudents = async (req, res, next) => {
 
 
 
-module.exports = { addScores,getScores, promoteStudents }
+module.exports = { addScores, getScores, promoteStudents }
 
+
+// const addScores = async (req, res, next) => {
+//   const { error } = addScoresValidation(req.body);
+//   if (error) throw error;
+
+//   const { admNo, firstName, lastName } = req.query;
+
+//   // check whether details match any student of the school
+//   const isStudent = await Student.findOne({
+//     $and: [{ admNo }, { firstName }, { lastName }]
+//   })
+//     .select("admNo firstName lastName");
+//   if (!isStudent) {
+//     return next(new Error("Error: no such student found"));
+//   }
+//   dbDebugger(isStudent._id)
+
+//   // check whether student exists in the scores database
+//   //if not, create a document for the student and push scores
+//   const alreadyHasScores = await Score.findOne({ studentId: isStudent._id })
+//   if (!alreadyHasScores) {
+//     const addStudent = await Score.create({ ...req.body, studentId: isStudent._id, AdmissionNumber: isStudent.admNo, Name: isStudent.firstName + " " + isStudent.lastName });
+//     addStudent.scores.push(req.body)
+//     addStudent.save()
+//     res.status(201).json({ status: "success", addStudent, msg: "student created and scores added successfully" });
+//   }
+//   else {// if yes, check whether student has scores for the session and term specified, display error if there is
+//     for (let count = 0; count < alreadyHasScores.scores.length; count++) {
+//       if (req.body.session === alreadyHasScores.scores[count].session && req.body.term === alreadyHasScores.scores[count].term) {
+//         dbDebugger("found ")
+//         return next(new Error("Error: scores for this term already exists for the student"));
+//       }
+//     }
+//     // if not, push the scores into the array of student's scores
+//     alreadyHasScores.scores.push(req.body)
+//     alreadyHasScores.save()
+//     res.status(201).json({ status: "success", msg: "scores for this student added successfully" });
+//   }
+// }
 
--- models/scoreModel.js
+++ models/scoreModel.js
@@ -34,264 +34,30 @@ const scoreSchema = new mongoose.Schema({
         },
         lowercase: true,
       },
-    subjects:{
-    hifz:
-        {
-            test:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            exam:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            total:Number,
-            grade:String
-        },
-    
-    fiqh:
-        {
-            test:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            exam:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            total:Number,
-            grade:String
-        },
-
-    tawheed:
-        {
-            test:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            exam:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            total:Number,
-            grade:String
-        },
-
-    hadith:
-        {
-            test:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            exam:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            total:Number,
-            grade:String
-        },
-
-    tajweed:
-        {
-            test:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            exam:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            total:Number,
-            grade:String
-        },
-
-    lugha:
-        {
-            test:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            exam:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            total:Number,
-            grade:String
-        } ,
-
-    tadribaatuLugha:
-        {
-            test:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            exam:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            total:Number,
-            grade:String
-        },
-
-    imlah:
-        {
-            test:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            exam:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            total:Number,
-            grade:String
-        },
-
-    adhkaar:
-        {
-            test:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            exam:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            total:Number,
-            grade:String
-        },
-
-    khatu:
-        {
-            test:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            exam:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            total:Number,
-            grade:String
-        },
-
-
-    taajiyya:
-        {
-            test:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            exam:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            total:Number,
-            grade:String
-        },
-
-    qiraaha:
-        {
-            test:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            exam:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            total:Number,
-            grade:String
-        },
-
-    nahw:
-        {
-            test:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            exam:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            total:Number,
-            grade:String
-        },
-
-    sarf:
-        {
-            test:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            exam:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            total:Number,
-            grade:String
-        },
-
-    tafseer:
-        {
-            test:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            exam:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            total:Number,
-            grade:String
-        },
-
-    tahbeer:
-        {
-            test:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            exam:{
-                type:Number,
-                trim:true,
-                maxlength:[2, 'score cannot be more than 2 characters']
-            },
-            total:Number,
-            grade:String
-        }
-    },
+    subjects:[{
+         subjectName:{
+              type:String,
+              trim:true,
+              required: [true, "subject cannot be empty"],
+              enum: {
+                values: ["fiqh", "hadith", "tawheed", "tafseer", "tadribaatuLugha", "lugha", "imlah", "hifz", "tajweed", "tahbeer", 
+                "taajiyya", "qiraaha", "khatu", "adhkaar", "nahw", "sarf"],
+                message: "{VALUE} is not supported, input a valid subject",
+              },
+               },
+         testScore:{
+              type:Number,
+              trim:true,
+              required: [true, "test score cannot be empty"],
+               maxlength:[2, 'score cannot be more than 2 characters']
+               },
+         examScore:{
+              type:Number,
+              trim:true,
+              required: [true, "exam score cannot be empty"],
+               maxlength:[2, 'score cannot be more than 2 characters']
+               }
+    }],
    
     totalScore: Number,
     avgPercent: Number,
@@ -303,3 +69,268 @@ const scoreSchema = new mongoose.Schema({
 
 
 module.exports = mongoose.model('Scores', scoreSchema)
+
+
+
+// subjects:{
+//     hifz:
+//         { name:{
+//             type:Number,
+//             trim:true,
+//             maxlength:[2, 'score cannot be more than 2 characters']
+//         },
+//             test:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             exam:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             total:Number,
+//             grade:String
+//         },
+    
+//     fiqh:
+//         {
+//             test:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             exam:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             total:Number,
+//             grade:String
+//         },
+
+//     tawheed:
+//         {
+//             test:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             exam:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             total:Number,
+//             grade:String
+//         },
+
+//     hadith:
+//         {
+//             test:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             exam:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             total:Number,
+//             grade:String
+//         },
+
+//     tajweed:
+//         {
+//             test:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             exam:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             total:Number,
+//             grade:String
+//         },
+
+//     lugha:
+//         {
+//             test:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             exam:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             total:Number,
+//             grade:String
+//         } ,
+
+//     tadribaatuLugha:
+//         {
+//             test:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             exam:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             total:Number,
+//             grade:String
+//         },
+
+//     imlah:
+//         {
+//             test:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             exam:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             total:Number,
+//             grade:String
+//         },
+
+//     adhkaar:
+//         {
+//             test:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             exam:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             total:Number,
+//             grade:String
+//         },
+
+//     khatu:
+//         {
+//             test:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             exam:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             total:Number,
+//             grade:String
+//         },
+
+
+//     taajiyya:
+//         {
+//             test:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             exam:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             total:Number,
+//             grade:String
+//         },
+
+//     qiraaha:
+//         {
+//             test:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             exam:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             total:Number,
+//             grade:String
+//         },
+
+//     nahw:
+//         {
+//             test:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             exam:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             total:Number,
+//             grade:String
+//         },
+
+//     sarf:
+//         {
+//             test:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             exam:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             total:Number,
+//             grade:String
+//         },
+
+//     tafseer:
+//         {
+//             test:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             exam:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             total:Number,
+//             grade:String
+//         },
+
+//     tahbeer:
+//         {
+//             test:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             exam:{
+//                 type:Number,
+//                 trim:true,
+//                 maxlength:[2, 'score cannot be more than 2 characters']
+//             },
+//             total:Number,
+//             grade:String
+//         }
+//     },
