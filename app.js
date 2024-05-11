b4f5b515ece40a8d2d422d4b1a8f12fff3dd0930
Nurat Lawal <nurahawwal@yahoo.com>
2024-05-11 19:52:14 +0100

    added user routes and controller

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const startupDebugger = require('debug')('app:startup')   // replaces console.lo
 // const dbDebugger = require('debug')('app:db')
 const Joi = require("joi");
 Joi.objectId = require('joi-objectid')(Joi);
+const jwt = require('jsonwebtoken')
+const cors = require('cors');
+
 const connectDB = require('./db/connect')
 const studentRouter = require('./routers/studentRouter');
 const scoreRouter = require('./routers/scoreRouter');
@@ -13,20 +16,32 @@ const errorHandler= require('./middleware/errorHandler')
 const morgan = require('morgan')
 const express = require('express');
 const app = express();
-const jwt = require('jsonwebtoken')
+
 app.set('view-engine', 'pug')
 app.set('views', './views')
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+app.use(cors());
 app.use(express.static('public'))
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
 
 
 if (app.get('env') === 'development' ) {
     app.use(morgan('tiny'))
     startupDebugger('morgan enabled...')
 }
+
+app.options('*', cors())
 app.use('/', homeRouter )
 app.use('/api/v1/user', userRouter )
 app.use('/api/v1/student', studentRouter )
