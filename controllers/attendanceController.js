3fc6d22d5b3c2b6c170cedc281ea7e451d09faf2
Nurat Lawal <nurahawwal@yahoo.com>
2024-09-30 18:42:06 +0100

    view attendance for a single student added

--- controllers/attendanceController.js
+++ controllers/attendanceController.js
@@ -1,4 +1,6 @@
 const Attendance = require("../models/attendanceModel");
+const Staff = require("../models/staffModel");
+const Student = require("../models/studentModel");
 const { BadUserRequestError, NotFoundError, UnAuthorizedError } =
     require('../middleware/errors');
 
@@ -20,7 +22,7 @@ const markAttendance = async (req, res, next) => {
                         { programme }
                     ]
             })
-            if (!studentExists) {
+        if (!studentExists) {
             const addStudent = await Attendance.create({ admissionNumber: req.body[count].admissionNumber, student_name: req.body[count].student_name, programme });
 
             let timeOfYear = {
@@ -77,29 +79,14 @@ const markAttendance = async (req, res, next) => {
     return res.status(200).json({ status: "Success", message: `attendance updated successfully` });
 }
 
-
 const getAttendance = async (req, res, next) => {
     const { termName, sessionName, className, programme } = req.query;
 
     if (req.user.role == "admin") {
         const isValidStaff = await Staff.findOne({ email: req.user.email })
-        if (isValidStaff.teacherProgramme != isStudent.programme) {
+        if (isValidStaff.teacherProgramme != programme) {
             throw new UnAuthorizedError("Error: Sorry, you are not allowed to view attendance for students of other programmes")
         }
-    }
-    if (req.user.role == "parent") {
-        if (req.user.email != isStudent.parentEmail)
-            throw new BadUserRequestError("Error: you do not have access to this report. Input your ward's admission number");
-    }
-    if (req.user.other_role == "parent") {
-        const isSameClass = await Staff.findOne({ email: req.user.email })
-        if (isSameClass.teacherClass != isStudent.presentClass && req.user.email != isStudent.parentEmail)
-            throw new BadUserRequestError("Error: you do not have access to this report. You're only able to view for your ward(s) or your students");
-    }
-    if (req.user.role == "student") {
-        const isValidStudent = await Student.findOne({ email: req.user.email })
-        if (admNo != isValidStudent.admNo)
-            throw new BadUserRequestError("Error: you do not have access to this report. Input your admission number.");
     }
 
     const attendanceExists = await Attendance.find({
@@ -124,6 +111,37 @@ const getAttendance = async (req, res, next) => {
         termName,
         className,
         sessionName,
+        attendanceExists
+    });
+}
+
+const getOneAttendance = async (req, res, next) => {
+    const {admissionNumber, termName, sessionName } = req.query;
+    const isStudent = await Student.findOne({ admNo: admissionNumber })
+
+    if (req.user.role == "parent") {
+        if (req.user.email != isStudent.parentEmail)
+            throw new BadUserRequestError("Error: you do not have access to this report. Input your ward's admission number");
+    }
+    if (req.user.other_role == "parent") {
+        const isSameClass = await Staff.findOne({ email: req.user.email })
+        if (isSameClass.teacherClass != isStudent.presentClass && req.user.email != isStudent.parentEmail)
+            throw new BadUserRequestError("Error: you do not have access to this report. Ensure you have inputted the correct admission number");
+    }
+
+    const attendanceExists = await Attendance.findOne({ admissionNumber })
+    if (!attendanceExists) throw new NotFoundError("Error: the requested attendance does not exist");
+    //get attendance
+    let studentAtd = attendanceExists.attendanceRecord // attendance record for the student for all sessions available
+    let sessionAttendance = studentAtd.find(asession => asession.sessionName == sessionName)
+    if (!sessionAttendance) throw new NotFoundError("Error: attendance no longer exists for this session");
+    let termRequested = sessionAttendance.term.find(aterm => aterm.termName == termName)
+    if (!termRequested) throw new NotFoundError("Error: attendance does not exist for this term");
+    if (termRequested.attendance.length == 0) throw new NotFoundError("Error: no attendance has been recorded for this term");
+
+    res.status(200).json({
+        status: "success",
+        message: "Successful",
         attendanceExists
     });
 }
@@ -173,7 +191,7 @@ const editAttendanceDate = async (req, res, next) => {
 const editAttendanceStatus = async (req, res, next) => {
     const { termName, sessionName, className, programme, admissionNumber } = req.query;
     const { attendanceDate } = req.body;
-   
+
     if (req.user.role == "admin") {
         const isValidStaff = await Staff.findOne({ email: req.user.email })
         if (isValidStaff.teacherProgramme != isStudent.programme) {
@@ -189,22 +207,22 @@ const editAttendanceStatus = async (req, res, next) => {
             { "attendanceRecord.className": className },
         ]
     })
-    console.log(attendanceDate)
+
     if (!attendanceExists) throw new NotFoundError("Error: the requested attendance does not exist");
     //get attendance
-        let studentAtd = attendanceExists.attendanceRecord // attendance record for the student for all sessions available
-        let sessionAttendance = studentAtd.find(asession => asession.sessionName == sessionName)
-        let termRequested = sessionAttendance.term.find(aterm => aterm.termName == termName)
-        if (termRequested) {
-            let daterequest = termRequested.attendance.find(adate => adate.termdate == attendanceDate)
-            console.log(daterequest)
-            // if date does not exist in student attendance record
-            if (!daterequest) throw new NotFoundError("Error: date cannot be found in the register");
-           if (daterequest.presence == "yes") daterequest.presence = "no"
-           else if (daterequest.presence == "no") daterequest.presence = "yes"
-            attendanceExists.save()
-        }
-   
+    let studentAtd = attendanceExists.attendanceRecord // attendance record for the student for all sessions available
+    let sessionAttendance = studentAtd.find(asession => asession.sessionName == sessionName)
+    let termRequested = sessionAttendance.term.find(aterm => aterm.termName == termName)
+    if (termRequested) {
+        let daterequest = termRequested.attendance.find(adate => adate.termdate == attendanceDate)
+        console.log(daterequest)
+        // if date does not exist in student attendance record
+        if (!daterequest) throw new NotFoundError("Error: date cannot be found in the register");
+        if (daterequest.presence == "yes") daterequest.presence = "no"
+        else if (daterequest.presence == "no") daterequest.presence = "yes"
+        attendanceExists.save()
+    }
+
     res.status(200).json({
         status: "success",
         message: `The student's attendance status for ${attendanceDate} is successfully changed`,
@@ -213,7 +231,7 @@ const editAttendanceStatus = async (req, res, next) => {
 }
 
 const deleteDayAttendance = async (req, res, next) => {
-    const {className, termName, sessionName, programme, atdDate } = req.query;
+    const { className, termName, sessionName, programme, atdDate } = req.query;
 
     if (req.user.role == "admin") {
         const isValidStaff = await Staff.findOne({ email: req.user.email })
@@ -293,4 +311,5 @@ const deleteTermAttendance = async (req, res, next) => {
 }
 
 
-module.exports = { markAttendance, getAttendance, editAttendanceDate, editAttendanceStatus, deleteDayAttendance, deleteTermAttendance }
+
+module.exports = { markAttendance, getAttendance,  getOneAttendance, editAttendanceDate, editAttendanceStatus, deleteDayAttendance, deleteTermAttendance }
--- routers/attendanceRouter.js
+++ routers/attendanceRouter.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {getAttendance, markAttendance, editAttendanceDate, editAttendanceStatus, deleteDayAttendance, deleteTermAttendance} 
+const {getAttendance, getOneAttendance, markAttendance, editAttendanceDate, editAttendanceStatus, deleteDayAttendance, deleteTermAttendance} 
        = require('../controllers/attendanceController')
 const authenticateUser = require('../middleware/auth')
 const {superAdmin, admin, adminORteacher} = require('../middleware/roles')
 
 
 router.route('/viewAttendance').get([authenticateUser], getAttendance)
+router.route('/viewOneAttendance').get([authenticateUser], getOneAttendance)
 router.route('/markAttendance').post([authenticateUser, adminORteacher], markAttendance)
 router.route('/changeAttendanceDate').patch([authenticateUser, adminORteacher], editAttendanceDate)
 router.route('/changeAttendanceStatus').patch([authenticateUser, adminORteacher], editAttendanceStatus)
